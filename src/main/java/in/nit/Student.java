ac9d6598eebe6f9b704a3fdda5c73bc258b26201
Rmnaresh <Rmnaresh@Rmnaresh-PC>
2020-02-22 12:59:40 +0530

    2nd Commit

--- src/main/java/in/nit/Student.java
+++ src/main/java/in/nit/Student.java
@@ -1,13 +1,21 @@
 package in.nit;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
-
-
+@Entity
+@Table(name="sttab")
 public class Student {
 
+	@Id
+	@Column(name="sid")
 	private Integer sid;
 
+	@Column(name="sname")
 	private String sname;
+	@Column(name="sfee")
 	private double sfee;
 	
 }
